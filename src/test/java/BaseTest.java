1bc3b8ae9de8366f9bf928380adbcfeeb02ea938
mwojciechowski2 <mateuszwojciechowski89@gmail.com>
2020-10-28 19:59:42 +0100

    First commit

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,42 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+    protected WebDriver driver;
+    protected Actions actions;
+    protected WebDriverWait wait;
+
+    By privacyPolcyAcceptanceButton = By.cssSelector("button[data-role='accept-consent']");
+
+    @BeforeEach
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+
+        driver.manage().window().setSize(new Dimension(1300, 800));
+        driver.manage().window().setPosition(new Point(10, 30));
+        actions = new Actions(driver);
+        driver.navigate().to("https://allegro.pl/");
+        driver.findElement(privacyPolcyAcceptanceButton).click();
+
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @AfterEach
+    public void driverQuit() {
+        driver.quit();
+    }
+}
+
+
