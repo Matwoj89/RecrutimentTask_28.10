8330dfe2de1fb75132e995afd6a579e2c72d321d
mwojciechowski2 <mateuszwojciechowski89@gmail.com>
2020-11-02 13:57:27 +0100

    Refactor 5 Update: maxPriceLocator, spinnerLoaderLocator - better selector getAndFormatPrice() - optimization method

--- src/test/java/ProductTest.java
+++ src/test/java/ProductTest.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 
 import java.text.DecimalFormat;
-import java.time.Duration;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -20,9 +19,9 @@ public class ProductTest extends BaseTest {
     private By blackColorLocator = By.xpath(".//label[contains(., 'czarny')]");
     private By smartphoneLocator = By.cssSelector("a[data-key='Elektronika'] + ul>li[data-role='LinkItem']>a");
     private By colorFilterLocator = By.cssSelector("h3[data-slot='Kolor']");
-    private By maxPriceLocator = By.cssSelector("article[data-analytics-view-custom-index0='2']>div>div+div>div+div>div>div>span");
+    private By maxPriceLocator = By.cssSelector("article[data-analytics-view-custom-index0='3']>div>div+div>div+div>div>div>span");
     private By productsLocators = By.cssSelector("article[data-analytics-view-custom-index0]");
-    private By spinnerLoaderLocator = By.cssSelector("a[name='allegro-spinner']");
+    private By spinnerLoaderLocator = By.cssSelector("div[class='_e219d_3uTGB']");
     private By dropdownListLocator = By.cssSelector("select[data-value='m']");
 
     @Test
@@ -69,6 +68,7 @@ public class ProductTest extends BaseTest {
         WebElement colorFilterElement = driver.findElement(colorFilterLocator);
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", colorFilterElement);
         driver.findElement(blackColorLocator).click();
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(spinnerLoaderLocator));
     }
 
     private void getProductQuantity() {
@@ -80,26 +80,26 @@ public class ProductTest extends BaseTest {
         WebElement dropdownList = driver.findElement(dropdownListLocator);
         Select sortDropdownList = new Select(dropdownList);
         sortDropdownList.selectByIndex(2);
-        WebElement allegroSpinnerLoader = driver.findElement(spinnerLoaderLocator);
-        wait.until(ExpectedConditions.invisibilityOf(allegroSpinnerLoader));
-        wait.withTimeout(Duration.ofSeconds(2));
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(spinnerLoaderLocator));
     }
 
     private void getAndFormatPrice() {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(maxPriceLocator));
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(spinnerLoaderLocator));
+
         WebElement maxPriceElement = driver.findElement(maxPriceLocator);
         String priceFromPage = maxPriceElement.getText();
 
-        String part1 = priceFromPage.substring(0, 1);
-        String part2 = priceFromPage.substring(2, 5);
-        String price = part1 + part2;
+        String priceToDoubleFormat = priceFromPage
+                .substring(0, priceFromPage.length() - 2)
+                .replaceAll("\\s","")
+                .replaceAll(",",".");
 
-        double priceWithoutVat = Integer.valueOf(price);
-        double finallyPrice = priceWithoutVat * 1.23;
-        DecimalFormat priceFormat = new DecimalFormat("#.00");
+        double priceWithoutVat = Double.parseDouble(priceToDoubleFormat);
+        double actualPrice = priceWithoutVat * 1.23;
+        DecimalFormat priceDecimalFormat = new DecimalFormat("#.00");
 
-        System.out.println("The price without 0.23 tax is: " + priceWithoutVat + " zł");
-        System.out.printf("The total price with added tax is: " + priceFormat.format(finallyPrice) + " zł");
+        System.out.printf("The price without 0.23 tax is: " + priceDecimalFormat.format(priceWithoutVat) + " zł\n");
+        System.out.printf("The total price with added tax is: " + priceDecimalFormat.format(actualPrice) + " zł");
 
     }
 }
